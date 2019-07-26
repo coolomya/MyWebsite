ca77b9cdabe52140f2ea5ebbdb4683c824c57777
Omey Bhosale <47236054+coolomya@users.noreply.github.com>
2019-07-25 20:54:23 -0500

    Added changing text for traits which fixes #2
    To change delay, change:
    -1) in animation in style.css
    -2) changtraits function in main.js
    
    *or read animation delay from css in js

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 //skiils array, add skills and level as needed
 var skills=[["java",9],["cpp",8],["python",8],["css3",5],["html5",3]]
 
+//traits array, add traits as needed
+var traits=["Developer", "Designer", "Photographer", "Learner", "Versatile", "Adaptable", "Dependable", "Funny", "Hardworking", "Friendly", "Diligent"];
+
+//bottom margin calculation
+var margin_bot = (10/(skills.length +1)).toString(10).concat("vh");
+
 //looping through skills and 
 // creating div elements inside skills-graph id
 // with dynamic width based on skills and 
 // dynamic bottom margin based on number of skills
-var margin_bot = (10/(skills.length +1)).toString(10).concat("vh");
 
 for (var i = 0; i < skills.length ; i++) {
 	var div = document.createElement("div");
@@ -22,6 +27,50 @@ for (var i = 0; i < skills.length ; i++) {
 	document.getElementById("skills-title").appendChild(div2);
 }
 
+/*Interval for text change loop*/
+var t=window.setInterval(changeTraits,4000);
+console.log('interval started');
+//var od=new Date().getTime();
+var i=1;
+function changeTraits(){
+	if(i>=traits.length){
+		i=0;
+	}
+	document.getElementById("second-text").innerHTML=traits[i++];
+	//var nd=new Date().getTime();
+	//console.log(Math.abs(nd - od));
+	//od=nd;
+	return null;
+}
+
+
+/*var e = document.getElementById('second-text');
+
+function whichAnimationEvent(){
+  var t;
+  var el = document.createElement("fakeelement");
+  var animations = {
+    "animation"      : "animationend",
+    "OAnimation"     : "oAnimationEnd",
+    "MozAnimation"   : "animationend",
+    "WebkitAnimation": "webkitAnimationEnd"
+  }
+
+  for (t in animations){
+    if (el.style[t] !== undefined){
+      return animations[t];
+    }
+  }
+}
+window.onload=function(){
+	var transitionEvent = whichAnimationEvent();
+	transitionEvent && e.addEventListener(transitionEvent, function() {
+	console.log('Transition complete!  This is the callback, no library needed!');
+});
+
+}*/
+
+/* Listen for a transition! */
 
 /*
 div.style.margin-top=10;
